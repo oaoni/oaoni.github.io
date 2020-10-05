85599636c3887c708ec861f0ec26fb15dd1d606f
Oni <onio@mski2314.mskcc.root.mskcc.org>
2020-10-05 13:11:22 -0400

    Updates

---
+++ precache-manifest.5bcecd90f8d24302d71d476ef098bbd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f4ca2118a271bfb2dd290d05f8ea30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7bbcb004ce5b9c3a594",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "88da1f9667c22fd05627",
+    "url": "/static/css/main.63aeabae.chunk.css"
+  },
+  {
+    "revision": "a7bbcb004ce5b9c3a594",
+    "url": "/static/js/2.69a0c609.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.69a0c609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88da1f9667c22fd05627",
+    "url": "/static/js/main.c55f0c4d.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "10edf13781f6f0ccb20c8263af319c26",
+    "url": "/static/media/profile.10edf137.png"
+  },
+  {
+    "revision": "b06bf68923f709cb1c4b110bb46f70f8",
+    "url": "/static/media/resume.b06bf689.pdf"
+  }
+]);
